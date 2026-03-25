d5814a32b4a1adb981c09164909f9b66c6e16afb
yoonjung561 <kyoonjung0601@gmail.com>
2026-03-25 11:03:37 +0900

    Initial Commit

---
+++ FindingLearners/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FindingLearners
+//
+//  Created by YoonJung Kwak on 3/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FindingLearners/FindingLearnersApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindingLearnersApp.swift
+//  FindingLearners
+//
+//  Created by YoonJung Kwak on 3/25/26.
+//
+
+import SwiftUI
+
+@main
+struct FindingLearnersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
